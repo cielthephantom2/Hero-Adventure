ded222baab4a0f89544c3f6f49c90aab862e45ff
cielthephantom2 <cielthephantom2@gmail.com>
2025-07-16 16:09:55 +0700

    update
    update

---
+++ Wulin-VN/obj/Release/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Vyyy.Translation
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Vyyy.Translation";
+        public const string PLUGIN_NAME = "Vyyy.Translation";
+        public const string PLUGIN_VERSION = "1.0.4";
+    }
+}
